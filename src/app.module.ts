2d19540e1c48a169ce6d4918517cda74d2e6700c
romesh-CG <romeshperera97@gmail.com>
2026-07-04 13:05:08 +0100

    Added: mysql database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from './config/configuration';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('db.host'),
+        port: +config.get('db.port'),
+        username: config.get('db.username'),
+        password: config.get('db.password'),
+        database: config.get('db.database'),
+        autoLoadEntities: true,
+        synchronize: true,
+        extra: {
+          timezone: 'UTC',
+        },
+      }),
     }),
     AuthModule,
   ],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,4 +4,11 @@ export default () => ({
     port: process.env.PORT,
     secret: process.env.JWT_SECRET,
   },
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
 });
